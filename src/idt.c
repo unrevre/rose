a13781b898071d53bcabed767ef292cad1653e64
unrevre <unrevre@gmail.com>
2019-04-06 19:41:09 -0400

    idt: add gate type as argument to macro
    rename argument (interrupt > handler) for clarity

--- src/idt.c
+++ src/idt.c
@@ -32,34 +32,34 @@ void init_idt(void) {
     trap_gate.dpl = 0;
     trap_gate.present = 1;
 
-#define SET_IDT(index, interrupt)           \
-    idt[index] = interrupt_gate;            \
-    SET_IDT_ENTRY(idt[index], interrupt);
+#define SET_IDT(index, gate, handler)       \
+    idt[index] = gate;                      \
+    SET_IDT_ENTRY(idt[index], handler);
 
-    SET_IDT(0, divide_error);
-    SET_IDT(1, debug);
-    SET_IDT(2, nmi);
-    SET_IDT(3, breakpoint);
-    SET_IDT(4, overflow);
-    SET_IDT(5, bound_range_exceeded);
-    SET_IDT(6, invalid_opcode);
-    SET_IDT(7, device_not_available);
-    SET_IDT(8, double_fault);
+    SET_IDT(0, interrupt_gate, divide_error);
+    SET_IDT(1, interrupt_gate, debug);
+    SET_IDT(2, interrupt_gate, nmi);
+    SET_IDT(3, interrupt_gate, breakpoint);
+    SET_IDT(4, interrupt_gate, overflow);
+    SET_IDT(5, interrupt_gate, bound_range_exceeded);
+    SET_IDT(6, interrupt_gate, invalid_opcode);
+    SET_IDT(7, interrupt_gate, device_not_available);
+    SET_IDT(8, interrupt_gate, double_fault);
     // idt[9] is reserved
-    SET_IDT(10, invalid_tss);
-    SET_IDT(11, segment_not_present);
-    SET_IDT(12, stack_fault);
-    SET_IDT(13, general_protection);
-    SET_IDT(14, page_fault);
+    SET_IDT(10, interrupt_gate, invalid_tss);
+    SET_IDT(11, interrupt_gate, segment_not_present);
+    SET_IDT(12, interrupt_gate, stack_fault);
+    SET_IDT(13, interrupt_gate, general_protection);
+    SET_IDT(14, interrupt_gate, page_fault);
     // idt[15] is reserved
-    SET_IDT(16, floating_point_error);
-    SET_IDT(17, alignment_check);
-    SET_IDT(18, machine_check);
-    SET_IDT(19, simd_fp_exception);
+    SET_IDT(16, interrupt_gate, floating_point_error);
+    SET_IDT(17, interrupt_gate, alignment_check);
+    SET_IDT(18, interrupt_gate, machine_check);
+    SET_IDT(19, interrupt_gate, simd_fp_exception);
 
-    SET_IDT(IDT_PIT, pit);
-    SET_IDT(IDT_KBD, kbd);
-    SET_IDT(IDT_RTC, rtc);
+    SET_IDT(IDT_PIT, interrupt_gate, pit);
+    SET_IDT(IDT_KBD, interrupt_gate, kbd);
+    SET_IDT(IDT_RTC, interrupt_gate, rtc);
 
     (void)trap_gate;
 }
