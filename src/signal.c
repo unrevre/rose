97de817511d2b1f000ecf531287f1674fe65941d
unrevre <unrevre@gmail.com>
2019-12-19 18:17:55 +0800

    signal: save and restore caller-saved registers
    fix register values being clobbered in memcpy calls.

--- src/signal.c
+++ src/signal.c
@@ -51,18 +51,23 @@ void deliver_signal(void) {
                  leal   8(%%ebp), %%eax     \n\
                  movl   44(%%eax), %%ecx    \n\
                  subl   $0x8, %%ecx         \n\
+                 pushl  %%eax               \n\
                  pushl  $0x8                \n\
                  leal   _sigreturn, %%edx   \n\
                  pushl  %%edx               \n\
                  pushl  %%ecx               \n\
                  call   memcpy              \n\
+                 popl   %%ecx               \n\
+                 addl   $0x8, %%esp         \n\
+                 popl   %%eax               \n\
                  subl   $0x34, %%ecx        \n\
-                 addl   $0xc, %%esp         \n\
                  pushl  $0x34               \n\
                  pushl  %%eax               \n\
                  pushl  %%ecx               \n\
                  call   memcpy              \n\
-                 addl   $0xc, %%esp         \n\
+                 popl   %%ecx               \n\
+                 popl   %%eax               \n\
+                 addl   $0x4, %%esp         \n\
                  subl   $0x8, %%ecx         \n\
                  movl   %0, 4(%%ecx)        \n\
                  leal   60(%%ecx), %%edx    \n\
