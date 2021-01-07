a5a1ea36a6fc2174bd6c119a7ee3d2af0c015a38
unrevre <unrevre@gmail.com>
2021-01-07 16:43:20 -0700

    scripts: update debugging script
    gdb on x64 supports debugging of i386 executables. update terminal
    settings for linux.

--- scripts/attachgdb.sh
+++ scripts/attachgdb.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-if [ $(uname -s) = "Darwin" ]; then
-    vte=kitty
-    gdb=i386-elf-gdb
-else
-    vte='urxvt -e'
-    gdb=gdb
-fi
-
-$vte qemu-system-i386 -drive format=raw,file=./img/rose.iso \
-    -m 256 -no-shutdown -no-reboot -s -S -curses &
-sleep 2; $gdb ./img/rose -ex 'target remote localhost:1234'
+kitty --single-instance \
+    qemu-system-i386 -drive format=raw,file=./img/rose.iso \
+        -m 256 -no-shutdown -no-reboot -s -S -curses &
+sleep 2; gdb ./img/rose -ex 'target remote localhost:1234'
